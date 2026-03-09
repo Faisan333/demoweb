14e093ece8e0ebd4e333bece4d71faf3fa9dfd90
MUHAMMED FAISAN <azeezfaizan@gmail.com>
2026-03-09 13:15:51 +0400

    bismil

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+ // Back to top button
+ $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+
+  
+
+// slick slider
+$('.chocolate_container').slick({
+    infinite: true,
+    center: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    responsive: [{
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
+
+/** google_map js **/
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+function closemodal(){
+    document.getElementById('id01').style.display='none'
+}
+
+
+
